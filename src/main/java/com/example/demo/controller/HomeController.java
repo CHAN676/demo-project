08bbcb0e4759c13a6106b8f95164821d834cfb3e
desigan143 <desigand415@gmail.com>
2026-03-20 23:15:25 +0530

    fixed home mapping

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "home";   // home.html
+        return "students"; // your main page (students.html)
     }
 }
