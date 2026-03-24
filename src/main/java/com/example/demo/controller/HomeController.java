4dcaa246c4d30d423881475a2c45f9cb8481f609
desigan143 <desigand415@gmail.com>
2026-03-24 16:50:30 +0530

    Fixed controller error

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,11 @@
-package com.example.demo.controller;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-@Controller
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String home() {
-        return "App is running 🚀";
+        return "SUCCESS 🚀 APP WORKING";
     }
 }
