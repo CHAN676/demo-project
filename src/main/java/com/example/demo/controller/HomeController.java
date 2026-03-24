68708af25b408997d63d0d9e1a036e847470fcca
desigan143 <desigand415@gmail.com>
2026-03-24 16:43:39 +0530

    Fix 502 issue + controller

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,12 +2,12 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
 public class HomeController {
 
     @GetMapping("/")
+    @ResponseBody
     public String home() {
-        return "home";
+        return "App is running 🚀";
     }
 }
